17b3dea6453df4eceac41d4888c71f55c2f5e34e
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2020-04-28 07:12:04 +0200

    First exercise: Create your first function
    I have included a Toast message in MainActivity onCreate() method. The goal is to create a function to make the toast.

--- first-app/app/src/main/java/com/icabanas/android/first_app/MainActivity.kt
+++ first-app/app/src/main/java/com/icabanas/android/first_app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.icabanas.android.first_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show()
     }
 }
